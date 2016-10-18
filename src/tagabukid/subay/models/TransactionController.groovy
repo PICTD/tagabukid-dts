31b015d404ea76ff14ba2a1891eab7e07a917da0
RUFINO <aguilarufino@gmail.com>
2016-10-18 19:50:32 +0800

    change name of models

---
+++ src/tagabukid/subay/models/TransactionController.groovy
@@ -0,0 +1,259 @@
+import com.rameses.rcp.annotations.*
+import com.rameses.rcp.common.*
+import com.rameses.osiris2.client.*
+import com.rameses.osiris2.common.*
+import java.rmi.server.*;
+import com.rameses.util.*;
+import java.text.*;
+
+public class DTSProcessOnlineController {
+    @Binding
+    def binding;
+    
+    @Service("TagabukidDTSService")
+    def svc
+    
+    @Service("DateService")
+    def dtsvc
+    
+    def din;
+    def entity;
+    def document;
+    def mode = "INIT"
+    def selectedItem;
+    def stats;
+    @FormTitle
+    def title;
+    def completed;
+    
+    def df = new SimpleDateFormat("yyyy-MM-dd");
+    def formatDate = { o->
+        if(o instanceof java.util.Date) {
+            return df.parse( df.format( o ));
+        }
+        return df.parse( o );     
+    }
+    
+    void init(){
+        mode= "INIT"
+        title="Select Transaction Mode"
+        document =[];
+        din = "";
+        entity = [:]
+        listHandler.reload();
+        stats = svc.getStats();
+        entity.txndate = dtsvc.getServerDate();
+        completed = false;
+    }
+    
+    def createnew(){
+        init();
+        return "default";
+    }
+    
+    def listHandler = [
+        fetchList : { return document },
+        onRemoveItem : {
+            if (MsgBox.confirm('Delete item?')){                
+                document.remove(it)
+                listHandler.reload();
+                return true;
+            }
+            return false;
+        },
+        onColumnUpdate:{item,colName ->
+            document.each{y ->
+                if (item.din == y.din){
+                    y.message = item.remarks
+                }
+            }
+        }
+    ] as EditorListModel
+    
+    def addtolist(def doc){
+        checkDuplicate(document,doc);
+        document << doc;
+        din = ""
+//        binding.refresh('din');
+//        binding.focus('din');
+        listHandler.reload();
+    }
+    
+    def searchdocument(){
+        def doc = svc.lookupDIN(entity)
+        if (!doc){
+            throw new Exception("Invalid DIN");
+        }
+        
+        if (doc.size == 1){
+            addtolist(doc[0])
+        }
+        else if(doc.size > 1){
+            return Inv.lookupOpener('din:lookup',[
+                    entity: entity,
+                    onselect :{
+                        addtolist(it)
+                    }
+                ])
+        }else{
+            throw new Exception("Invalid DIN");
+        }
+    }
+
+    public def barcodesearch(){
+        //        if (!din){
+        //            throw new Exception("Invalid DIN");
+        //        }
+        entity.searchtext = din
+        entity.mode = mode
+        entity.document = document
+        searchdocument()
+    }
+    
+
+    void receive (){
+        mode = "receive"
+        title= "RECEIVING"
+        binding.refresh('din');
+        binding.focus('din');
+    }
+    void outgoing (){
+        mode = "outgoing"
+        title= "OUTGOING"
+        entity.sendtype = 'localoutgoing'
+        binding.refresh('din');
+        binding.focus('din');
+    }
+    void send (){
+        mode = "send"
+        title= "SENDING"
+        entity.sendtype = 'local'
+        binding.refresh('din');
+        binding.focus('din');
+    }
+    void archived (){
+        mode = "archived"
+        title= "ARCHIVING"
+        binding.refresh('din');
+        binding.focus('din');
+    }
+   
+    def getHandler(){
+        if(mode!="INIT"){
+            if(mode=="send" || mode=="outgoing"){
+                return InvokerUtil.lookupOpener(entity.sendtype+':form',[
+                        entity: entity
+                    ]) 
+            }else{
+                return InvokerUtil.lookupOpener(mode+':form',[
+                        entity: entity
+                    ])  
+            }
+            
+        }
+    }
+    
+    def save(){
+        
+        if (!entity.org && !entity.destinations && mode.matches('send|archived')) 
+        throw new Exception("Destination is Required");
+        
+        if (!entity.assignee && !entity.oic && mode=='send') 
+        throw new Exception("OIC is Required");
+        
+        if (document.size == 0)
+        throw new Exception("Please add at least 1 document to process");
+        
+        entity.document = document
+        entity.mode = mode
+        if( MsgBox.confirm( "You are about to " + mode + " this transaction. Proceed?")) {
+            try{
+                svc.processDocument(entity)
+                MsgBox.alert("Transaction Successfull")
+                completed = true;
+                
+            } catch(Warning w) {
+                Modal.show( 'document_redflag:warning', [list: w.info.list] );
+                throw new BreakException();
+            } catch(e) {
+                throw e;
+            }
+        }
+       return "completed"
+        
+    }
+    
+    def getInfoHtml() {
+        return TemplateProvider.instance.getResult( "tagabukid/dts/dtstransaction.gtpl", [entity:entity] );
+    }
+    
+    def findReportOpener( reportData ) { 
+        //check first if form handler exists. 
+        def o = InvokerUtil.lookupOpener( "dtstransaction:report", [reportData:reportData] );
+        if ( !o ) throw new Exception("Handler not found"); 
+
+        return o.handle; 
+    } 
+    void print() {
+        def handle = findReportOpener(entity);
+        handle.viewReport();
+        ReportUtil.print(handle.report,true);
+    }
+    
+    void checkDuplicate(listtofilter,item){
+        def data = listtofilter.find{it.din == item.din }
+        if (data)
+        throw new Exception("Duplicate item is not allowed.")
+    }
+    
+    def close(){
+        return '_close'
+    }
+    
+    //    def processDocument(){
+    //        if(mode.matches('send|archived')){
+    //          
+    //        }
+    //        
+    //        println entity
+    //        //createtask(mode)
+    //      
+    //        //if outgoing
+    //        //end prev task
+    //        //create new outgoing task
+    //        //org = current user organization
+    //        //assignee = current user
+    //        //actor = current user
+    //        
+    //        //if sending
+    //        //ask destination/org
+    //        //ask assignee
+    //        //actor = current user
+    //        //end prev task
+    //        //create new enroute task
+    //        
+    //        //if receiving
+    //        //assignee = user
+    //        //actor = user
+    //        //org = current user org
+    //        //end prev task
+    //        //create new processing task
+    //        
+    //        //if archived
+    //        //ask archived destination
+    //        //ask cabinet/folder no/catalog
+    //        //assignee = current user
+    //        //actor = current user
+    //        //org = current user org
+    //        //end prev task
+    //        //create new archived task
+    //      
+    ////        return Inv.lookupOpener('din:lookup',[
+    ////                entity: entity,
+    ////                onselect :{
+    ////                    addtolist(it)
+    ////                }
+    ////            ])
+    //        //svc.processDocument(entity)
+    //    }
+}
